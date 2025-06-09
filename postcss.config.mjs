5af2cd114c2f069e16c3ef4542667191a7585e6b
fedesuarez16 <fedesuarez16@gmail.com>
2025-06-08 23:07:40 -0300

    idk

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,11 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     '@tailwindcss/postcss': {
       config: {
-        plugins: [require('@tailwindcss/typography')],
+        plugins: [typography],
       },
     },
   },
